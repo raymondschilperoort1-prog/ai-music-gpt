a164c401309f6f6a5a13233495135c9a9b46f91a
raymondschilperoort1-prog <raymond.schilperoort1@gmail.com>
2026-02-08 20:27:58 +0100

    Update main.py

--- music-service/main.py
+++ music-service/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from audiocraft.models import MusicGen
+import uuid
 
 app = FastAPI()
+
+print("Loading MusicGen model...")
+model = MusicGen.get_pretrained("facebook/musicgen-small")
+print("Model loaded!")
 
 @app.get("/")
 def home():
@@ -8,4 +14,11 @@ def home():
 
 @app.post("/generate-music")
 def generate_music():
-    return {"message": "Music service alive"}
+    prompt = ["romantic french chanson with piano"]
+
+    wav = model.generate(prompt)
+
+    filename = f"{uuid.uuid4()}.wav"
+    model.save_wav(wav, filename)
+
+    return {"file": filename}
